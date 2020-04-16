c72ce78cbf35f80604e752ab3dfb45024dfe7476
Tue Boesen <tue.boesen@gmail.com>
2020-04-16 11:58:12 -0700

    updated run_10000

--- run_10000.py
+++ run_10000.py
@@ -18,8 +18,9 @@ context = {
     'use_label_probabilities': True,                        #Switch the labels from onehot to a probabilities
     'batch_size': 16,
     #Supervised learning
-    'epochs_SL': 10,                                         #Epochs for supervised learning
+    'epochs_SL': 20,                                        #Epochs for supervised learning
     'lr': 1e-2,
+    'loss_type': 'MSE',                                     #Options are MSE and CE
 
     #Active learning
     'epochs_AL': 10,                                        #Iterations to use in Active learning
